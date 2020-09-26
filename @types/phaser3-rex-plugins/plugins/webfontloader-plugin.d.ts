cee6b123ca96aba334663b55181d4a8d8a432750
Bulat Khasanov <afti@yandex.ru>
2020-09-26 23:35:52 +0300

    Fix case of WebFontLoaderPlugin

--- @types/phaser3-rex-plugins/plugins/webfontloader-plugin.d.ts
+++ @types/phaser3-rex-plugins/plugins/webfontloader-plugin.d.ts
@@ -1,7 +1,7 @@
 declare module 'phaser3-rex-plugins/plugins/webfontloader-plugin' {
     import * as Phaser from 'phaser';
 
-    export default class WebfontLoaderPlugin extends Phaser.Plugins.BasePlugin {
+    export default class WebFontLoaderPlugin extends Phaser.Plugins.BasePlugin {
         constructor(pluginManager: Phaser.Plugins.PluginManager);
 
         addToScene(scene: Phaser.Scene): void;
