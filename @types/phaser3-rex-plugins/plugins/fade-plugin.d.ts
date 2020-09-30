74010bf717799be9720a7b49b8a69997c9de5176
Bulat Khasanov <afti@yandex.ru>
2020-10-01 01:31:27 +0300

    Add fadeOutDestroy method of plugin

--- @types/phaser3-rex-plugins/plugins/fade-plugin.d.ts
+++ @types/phaser3-rex-plugins/plugins/fade-plugin.d.ts
@@ -1,10 +1,13 @@
 declare module 'phaser3-rex-plugins/plugins/fade-plugin' {
     import * as Phaser from 'phaser';
     import {FadeConfig} from 'phaser3-rex-plugins/plugins/behaviors/fade/Fade';
+    import FadeOutDestroy from "phaser3-rex-plugins/plugins/fade-out-destroy";
 
     export default class FadePlugin extends Phaser.Plugins.BasePlugin {
         constructor(pluginManager: Phaser.Plugins.PluginManager);
 
         add(gameObject: Phaser.GameObjects.GameObject, config: FadeConfig): void;
+
+        fadeOutDestroy: typeof FadeOutDestroy;
     }
 }
