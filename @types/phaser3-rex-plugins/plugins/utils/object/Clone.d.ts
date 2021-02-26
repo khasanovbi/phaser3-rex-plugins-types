8bcf75d4cad7038135590a05bff80c36d53764b4
Bulat Khasanov <afti@yandex.ru>
2021-02-27 00:41:31 +0300

    Update Clone

--- @types/phaser3-rex-plugins/plugins/utils/object/Clone.d.ts
+++ @types/phaser3-rex-plugins/plugins/utils/object/Clone.d.ts
@@ -1,5 +1,3 @@
 declare module 'phaser3-rex-plugins/plugins/utils/object/Clone' {
-    import Clear from 'phaser3-rex-plugins/plugins/utils/object/Clear';
-
-    export default function Clone(obj: any, out: any): any;
+    export default function Clone<T = any>(obj: T, out?: any): T;
 }
