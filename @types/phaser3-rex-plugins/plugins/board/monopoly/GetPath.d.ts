25968acf5eddbabfbe027b28b4f2b8305d353179
Bulat Khasanov <afti@yandex.ru>
2021-02-26 21:17:17 +0300

    Update Monopoly

---
+++ @types/phaser3-rex-plugins/plugins/board/monopoly/GetPath.d.ts
@@ -0,0 +1,3 @@
+declare module 'phaser3-rex-plugins/plugins/board/monopoly/GetPath' {
+    export default function GetPath(movingPoints: any, out?: any[]): any[];
+}
--- @types/phaser3-rex-plugins/plugins/board/monopoly/Monopoly.d.ts
+++ @types/phaser3-rex-plugins/plugins/board/monopoly/Monopoly.d.ts
@@ -1,28 +1,56 @@
 declare module 'phaser3-rex-plugins/plugins/board/monopoly/Monopoly' {
-    export default class Monopoly {
-        constructor(gameObject: any, config: any);
+    import Methods from 'phaser3-rex-plugins/plugins/board/monopoly/Methods';
+
+    type MethodsType = typeof Methods;
+
+    interface MonopolyConfig {
+        preTileXY?: any;
+        costCallback?: Function;
+        costCallbackScope?: any;
+        cost?: number;
+        face?: number;
+        pathMode?: number;
+        pathTileZ?: number;
+    }
+
+    export default class Monopoly implements MethodsType {
         gameObject: any;
         chessData: any;
-        resetFromJSON(o: any): this;
         preTileXY: any;
-        boot(): void;
-        shutdown(): this;
-        destroy(): this;
-        setFace(direction: any): this;
         face: any;
-        setPathMode(mode: any): this;
         pathMode: any;
-        setCostFunction(callback: any, scope: any): this;
         costCallback: any;
         costCallbackScope: any;
-        setPathTileZ(value: any): this;
         pathTileZ: any;
+
+        constructor(gameObject: any, config: MonopolyConfig);
+
+        resetFromJSON(o: MonopolyConfig): this;
+
+        boot(): void;
+
+        shutdown(): this;
+
+        destroy(): this;
+
+        setFace(direction: any): this;
+
+        setPathMode(mode: any): this;
+
+        setCostFunction(callback?: Function, scope?: any): this;
+
+        setPathTileZ(value: any): this;
+
         get BLOCKER(): any;
+
         get STOP(): number;
+
         get board(): any;
 
-        getPath(movingPoints: any, out: any): any;
+        getCost(curTileXY: any, preTileXY: any): number;
+
         getNextTile(curTileData: any, preTileData: any): any;
-        getCost(curTileXY: any, preTileXY: any): any;
+
+        getPath(movingPoints: any, out?: any[]): any[];
     }
 }
