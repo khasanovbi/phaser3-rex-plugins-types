4e0f7335007b690806a6306d97aa3a5f8ce7c7d6
Bulat Khasanov <afti@yandex.ru>
2021-02-27 00:18:54 +0300

    Update Shuffle

--- @types/phaser3-rex-plugins/plugins/utils/array/Shuffle.d.ts
+++ @types/phaser3-rex-plugins/plugins/utils/array/Shuffle.d.ts
@@ -1,3 +1,3 @@
 declare module 'phaser3-rex-plugins/plugins/utils/array/Shuffle' {
-    export default function Shuffle(array: Array<any>): Array<any>;
+    export default function Shuffle<T = any>(array: T[]): T[];
 }
