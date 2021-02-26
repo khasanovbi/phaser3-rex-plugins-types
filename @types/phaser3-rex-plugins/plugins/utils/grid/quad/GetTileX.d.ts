6af49d900c345f3f4596250219c332ac43defdbd
Bulat Khasanov <afti@yandex.ru>
2021-02-27 00:40:25 +0300

    Update Quad

---
+++ @types/phaser3-rex-plugins/plugins/utils/grid/quad/GetTileX.d.ts
@@ -0,0 +1,3 @@
+declare module 'phaser3-rex-plugins/plugins/utils/grid/quad/GetTileX' {
+    export default function GetTileX(worldX: any, worldY: any): number;
+}
---
+++ @types/phaser3-rex-plugins/plugins/utils/grid/quad/GetTileXY.d.ts
@@ -0,0 +1,3 @@
+declare module 'phaser3-rex-plugins/plugins/utils/grid/quad/GetTileXY' {
+    export default function GetTileXY(worldX: any, worldY: any, out?: any): any;
+}
