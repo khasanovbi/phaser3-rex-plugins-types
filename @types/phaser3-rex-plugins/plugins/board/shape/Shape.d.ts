325b73157e97ba24ef68a667d6444e76dea77fc1
Bulat Khasanov <afti@yandex.ru>
2021-02-26 22:06:56 +0300

    Update Shape

--- @types/phaser3-rex-plugins/plugins/board/shape/Shape.d.ts
+++ @types/phaser3-rex-plugins/plugins/board/shape/Shape.d.ts
@@ -1,9 +1,10 @@
 declare module 'phaser3-rex-plugins/plugins/board/shape/Shape' {
+    import * as Phaser from 'phaser';
     import Board from 'phaser3-rex-plugins/plugins/board/board/Board';
 
-    export default class Shape {
+    export default class Shape extends Phaser.GameObjects.Polygon {
         constructor(
-            board?: Board,
+            board: Board,
             tileX?: number,
             tileY?: number,
             tileZ?: number,
