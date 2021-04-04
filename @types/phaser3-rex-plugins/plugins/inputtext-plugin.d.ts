c2316e1f5e67aa291f1f028476fae5589c0ebe9f
Bulat Khasanov <afti@yandex.ru>
2021-04-04 17:03:35 +0300

    Add inputtext plugin

---
+++ @types/phaser3-rex-plugins/plugins/inputtext-plugin.d.ts
@@ -0,0 +1,5 @@
+declare module 'phaser3-rex-plugins/plugins/inputtext-plugin' {
+    import * as Phaser from 'phaser';
+
+    export default class InputTextPlugin extends Phaser.Plugins.BasePlugin {}
+}
